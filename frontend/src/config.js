e7290c5c1da898762c940281a057b3e73e5ddb50
김진호 <rlawlsgh1227@gmail.com>
2023-01-30 12:13:21 +0900

    chore: React initial setting

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+const BASE_URL = process.env.REACT_APP_SERVER_HOST;
+
+export { BASE_URL };
---
+++ frontend/src/styles/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing:border-box;
+    }
+`;
+
+export default GlobalStyle;
