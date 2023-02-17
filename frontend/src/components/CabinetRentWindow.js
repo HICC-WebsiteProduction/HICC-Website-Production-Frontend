6da6d6eb929b6ab8522b03293ecc02c65aea4658
xman0425 <xman0425@naver.com>
2023-02-18 00:08:59 +0900

    feat: refined layout logic

--- frontend/src/components/CabinetRentWindow.js
+++ frontend/src/components/CabinetRentWindow.js
@@ -1,10 +1,12 @@
+import { faColumns } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import styled from 'styled-components';
 
 const pixelToRem = size => `${size / 16}rem`;
 
 const layoutColumn = 3;
-const layoutRow = 4;
+const layoutRow = 2;
+const layoutWidthPx = 608;
 
 function CabinetRentWindow(props) {
   return (
@@ -12,7 +14,11 @@ function CabinetRentWindow(props) {
       <CabinetLayoutDisplay>
         {Array.from(Array(layoutRow).keys()).map((value, index) => {
           return Array.from(Array(layoutColumn).keys()).map((value, index) => {
-            return <Cabinet key={value} />;
+            return (
+              <Cabinet key={value}>
+                <CabinetIcon />
+              </Cabinet>
+            );
           });
         })}
       </CabinetLayoutDisplay>
@@ -23,21 +29,30 @@ function CabinetRentWindow(props) {
 export default CabinetRentWindow;
 
 const CabinetRentContainer = styled.div`
-  border: 4px solid black;
+  display: flex;
 `;
 
 const CabinetLayoutDisplay = styled.div`
-  border: 4px solid red;
-  width: ${pixelToRem(608)};
+  width: ${pixelToRem(layoutWidthPx)};
+  margin: auto;
 `;
 
 const Cabinet = styled.div`
   float: left;
+  width: ${pixelToRem((layoutWidthPx - 8) / layoutColumn)};
+  height: ${pixelToRem((layoutWidthPx - 8) / layoutColumn)};
   background-color: pink;
-  width: ${pixelToRem(200)};
-  height: ${pixelToRem(200)};
 
   :nth-child(2n) {
     background-color: rgb(200, 255, 255);
   }
+`;
+
+// 브라우저 특화 css의 점유율은 아래 사이트 참고
+// https://caniuse.com/?search=-webkit-fill-available
+const CabinetIcon = styled.div`
+  margin: ${pixelToRem(8)};
+  height: -webkit-fill-available;
+  border: 4px solid brown;
+  border-radius: ${pixelToRem(20)};
 `;
