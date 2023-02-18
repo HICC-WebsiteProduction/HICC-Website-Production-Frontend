275f38304fc9424853c47937ab9185322dbe7983
Choi Seho <kindseho@naver.com>
2023-02-18 14:14:10 +0900

    testcommit

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Signup from './pages/Signup';
+import MyPage from './pages/MyPage';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
