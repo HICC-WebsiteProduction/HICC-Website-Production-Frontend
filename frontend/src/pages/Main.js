99de89979fa5908c97c93996c1b62790196da7cf
김진호 <rlawlsgh1227@gmail.com>
2023-07-18 16:42:00 +0900

    design: 우산 대여, 사물함 대여 페이지 프레임만 제작 완료
    중간에 pull 받다가 오류가 생겨 다시 클론하고 푸시하느라 변경사항이 많아보일 수 있습니다.
    메인페이지 코드를 현재 상황에 맞게 바꾸었습니다.
    이미지 파일들은 public에 넣어뒀습니다.
    useScrollCount 훅 useEffect 부분이 오류가 있어 수정했습니다

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import Header from '../components/header/Header';
-import logo from '../images/hicc_logo.png';
-import MainButton from './../components/util/MainButton';
-
-const pixelToRem = size => `${size / 16}rem`;
+import MainActivityContent from '../components/main/MainActivityContent';
+import logo from '../images/mainBanner.svg';
+import useScrollClipPath from './../hook/useScrollClipPath';
+import NumberTable from '../components/main/NumberTable';
 
 const boxAnimation = keyframes`
   0%{
@@ -18,63 +18,92 @@ const boxAnimation = keyframes`
 `;
 
 function Main() {
+  const animatedItem = useScrollClipPath('left', 1.5, 0);
+  const animatedItem2 = useScrollClipPath('top', 0.5, 0);
   return (
     <MainContainer>
       <Header />
-      <Logo />
-      <CentorContainer>
-        <ButtonContainer>
-          <MainButton buttonType="button" buttonName="동아리 소개"></MainButton>
-          <MainButton buttonType="button" buttonName="계시판"></MainButton>
-          <MainButton buttonType="button" buttonName="회계 정보"></MainButton>
-          <MainButton
-            buttonType="button"
-            buttonName="사물함/우산 대여"
-          ></MainButton>
-          <MainButton buttonType="button" buttonName="일정 캘린더"></MainButton>
-          <MainButton buttonType="button" buttonName="맛집 지도"></MainButton>
-        </ButtonContainer>
-      </CentorContainer>
+      <Banner>
+        <BannerTxt {...animatedItem}>
+          Hong Ik <br /> Computer Club
+        </BannerTxt>
+      </Banner>
+      <ActivityContainer>
+        <ActivityTitle>HICC는 이런 활동을 해요</ActivityTitle>
+        <ActivityContentContainer {...animatedItem2}>
+          <MainActivityContent buttonName="코딩 학술 세미나"></MainActivityContent>
+          <MainActivityContent buttonName="프로젝트 발표 대회"></MainActivityContent>
+          <MainActivityContent buttonName="HICC 정규모임"></MainActivityContent>
+          <MainActivityContent buttonName="분기 별 MT"></MainActivityContent>
+          <MainActivityContent buttonName="조별 미션 및 친목 활동"></MainActivityContent>
+          <MainActivityContent buttonName="그 외 활동"></MainActivityContent>
+        </ActivityContentContainer>
+      </ActivityContainer>
+      <NumberTable />
     </MainContainer>
   );
 }
 
 export default Main;
 
-const Logo = styled.div`
-  width: ${pixelToRem(300)};
-  height: ${pixelToRem(102)};
-  margin: 100px auto;
+const Banner = styled.div`
+  height: 100%;
+  margin: 0 auto;
   background-image: url('${logo}');
-  background-size: contain;
+  background-size: cover;
+  background-position: center center;
   background-repeat: no-repeat;
   grid-area: logo;
+  object-fit: contain;
   animation: ${boxAnimation} 0.2s linear alternate;
 `;
 
-const CentorContainer = styled.div`
-  display: flex;
-  place-items: center;
-  align-items: center;
-  justify-content: center;
+const BannerTxt = styled.div`
+  position: relative;
+  width: 100%;
+  top: 55%;
+  left: 20%;
+  font-size: xxx-large;
+  font-weight: bold;
+  color: #edf0f8;
 `;
 
-const ButtonContainer = styled.div`
+const ActivityContainer = styled.div`
+  position: relative;
+  width: 1920px;
+  height: 900px;
+  margin: 0 auto;
+`;
+
+const ActivityTitle = styled.div`
+  position: absolute;
+  top: 50px;
+  left: 363px;
+  width: 463px;
+  height: 48px;
+  margin-top: 50px;
+  font-size: xx-large;
+  font-weight: bold;
+  color: #edf0f8;
+`;
+
+const ActivityContentContainer = styled.div`
   display: grid;
+  grid-template-rows: repeat(2, minmax(300px, 300px));
+  grid-template-columns: repeat(3, minmax(378px, 378px));
+  gap: 30px;
+  position: absolute;
+  top: 178px;
+  left: 363px;
+  width: 1194px;
+  height: 630px;
+  align-items: center;
   place-items: center;
-  grid-area: main;
-  grid-template-rows: repeat(2, minmax(200px, 200px));
-  grid-template-columns: repeat(3, minmax(330px, 330px));
-  animation: ${boxAnimation} 0.5s linear alternate;
 `;
 
 const MainContainer = styled.div`
   height: 100vh;
-  grid-template-areas:
-    'header header header'
-    'logo logo logo'
-    'blank main blank';
-  grid-template-rows: 1fr 3fr 4fr;
-  grid-template-columns: 2fr 1fr 2fr;
+  width: 100%;
+  margin: 0 auto;
   place-items: center;
 `;
