3aead928104b02adeed0285e21c638183c40ac35
김진호 <rlawlsgh1227@gmail.com>
2023-07-21 16:52:09 +0900

    feat: 우산 대여 관리기능 생성
    우산 상태를 /umbrella endpoint를 통해 얻어옵니다.
    우산 분실 상태일 때 사용가능으로 바꾸면 사용 가능으로 돌아오고, 사용가능에서 분실 도난으로 바꾸면 사용 불가능으로 변경
    확인 버튼을 누르면 변경사항 백엔드로 날라가야하지만, 연동이 되지 않아 패스, 취소 누르면 reset되며, 다시 서버에서 불러온 값으로 변경

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,5 +1,11 @@
 import { authHandlers } from './handlers/auth';
 import { manageHandlers } from './handlers/manage';
 import { noticeHandlers } from './handlers/notice';
+import { umbrellaHandlers } from './handlers/umbrella';
 
-export const handlers = [...authHandlers, ...manageHandlers, ...noticeHandlers];
+export const handlers = [
+  ...authHandlers,
+  ...manageHandlers,
+  ...noticeHandlers,
+  ...umbrellaHandlers,
+];
