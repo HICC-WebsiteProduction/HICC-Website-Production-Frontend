b28d83453eb6716a06279b6057e33d8bbb759d56
Choi Seho <kindseho@naver.com>
2023-09-06 14:07:51 +0900

    feat: ckeditor5 적용

--- frontend/src/components/noticeboard/NewPost.js
+++ frontend/src/components/noticeboard/NewPost.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import theme from '../../styles/Theme';
 import dummy from '../../dummy/posts.json';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 // import ReactQuill from 'react-quill';
 // import 'react-quill/dist/quill.snow.css';
 
@@ -93,14 +95,25 @@ export default function NewPost(props) {
         <InputLabel>제목</InputLabel>
         <Input value={title} maxLength="50" onChange={handleTitleChange} />
       </InputBox>
-      <InputBox>
-        <InputLabel>내용</InputLabel>
-        <TextArea
-          value={content}
-          maxLength="1000"
-          onChange={handleContentChange}
-        />
-      </InputBox>
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p>Hello from CKEditor&nbsp;5!</p>"
+        onReady={editor => {
+          // You can store the "editor" and use when it is needed.
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log('Blur.', editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log('Focus.', editor);
+        }}
+      />
+
       <Button onClick={handleSaveClick}>저장</Button>
     </NewPostContainer>
   );
